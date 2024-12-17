077b5cb69f5cd4c674030b33265aa8503d02dd98
MALIK ABDUL BASIT <malikabdulbasit03@gmail.com>
2024-12-18 00:04:18 +0500

    Login $ Signup Screens Tutorial

---
+++ components/Account.tsx
@@ -0,0 +1,137 @@
+import { useState, useEffect } from 'react';
+import { supabase } from '@/lib/Supabase';
+import { StyleSheet, View, Alert, Text } from 'react-native';
+import { Button, Input } from '@rneui/themed';
+import { Session } from '@supabase/supabase-js';
+
+export default function Account({ session }: { session: Session }) {
+  const [loading, setLoading] = useState(true);
+  const [username, setUsername] = useState('');
+  const [fullName, setFullName] = useState('');
+  const [website, setWebsite] = useState('');
+  const [avatarUrl, setAvatarUrl] = useState('');
+  const [UserID, setUserID] = useState('');
+
+  useEffect(() => {
+    if (session) getProfile();
+  }, [session]);
+
+  async function getProfile() {
+    try {
+      setLoading(true);
+      if (!session?.user) throw new Error('No user on the session!');
+
+      const { data, error, status } = await supabase
+        .from('profiles')
+        .select(`username, website, avatar_url, id, full_name`)
+        .eq('id', session?.user.id)
+        .single();
+      
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setUsername(data.username);
+        setFullName(data.full_name);
+        setWebsite(data.website);
+        setAvatarUrl(data.avatar_url);
+        setUserID(data.id);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        Alert.alert(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function updateProfile({
+    username,
+    full_name,
+    website,
+    avatar_url,
+  }: {
+    username: string;
+    full_name: string;
+    website: string;
+    avatar_url: string;
+  }) {
+    try {
+      setLoading(true);
+      if (!session?.user) throw new Error('No user on the session!');
+
+      const updates = {
+        id: session?.user.id,
+        username,
+        full_name,
+        website,
+        avatar_url,
+        updated_at: new Date(),
+      };
+
+      const { error } = await supabase.from('profiles').upsert(updates);
+
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        Alert.alert(error.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Input label="Email" value={session?.user?.email} disabled />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input label="Username" value={username || ''} onChangeText={(text) => setUsername(text)} />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input label="Full Name" value={fullName || ''} onChangeText={(text) => setFullName(text)} />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <Input label="Website" value={website || ''} onChangeText={(text) => setWebsite(text)} />
+      </View>
+
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Button
+          title={loading ? 'Loading ...' : 'Update'}
+          onPress={() => updateProfile({ username, full_name: fullName, website, avatar_url: avatarUrl })}
+          disabled={loading}
+        />
+      </View>
+
+      <View style={styles.verticallySpaced}>
+        <Button title="Sign Out" onPress={() => supabase.auth.signOut()} />
+      </View>
+
+      <View style={styles.verticallySpaced}>
+        <Text>{username}</Text>
+        <Text>{UserID}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 12,
+  },
+  verticallySpaced: {
+    paddingTop: 4,
+    paddingBottom: 4,
+    alignSelf: 'stretch',
+
+  },
+  mt20: {
+    marginTop: 20,
+  },
+})
---
+++ components/WelcomeScreen.tsx
@@ -0,0 +1,73 @@
+import { useState, useEffect } from 'react';
+import { supabase } from '@/lib/Supabase';
+import { StyleSheet, View, Alert, Text } from 'react-native';
+import { Button, Input } from '@rneui/themed';
+import { Session } from '@supabase/supabase-js';
+
+export default function WelcomeScreen({ session }: { session: Session }){
+    
+    const [loading, setLoading] = useState(true);
+    const [username, setUsername] = useState('');
+    const [fullName, setFullName] = useState('');
+    const [website, setWebsite] = useState('');
+    const [avatarUrl, setAvatarUrl] = useState('');
+    const [UserID, setUserID] = useState('');
+    
+    useEffect(() => {
+        if (session) getProfile();
+      }, [session]);
+
+    async function getProfile() {
+        try {
+          setLoading(true);
+          if (!session?.user) throw new Error('No user on the session!');
+    
+          const { data, error, status } = await supabase
+            .from('profiles')
+            .select(`username, website, avatar_url, id, full_name`)
+            .eq('id', session?.user.id)
+            .single();
+          
+          if (error && status !== 406) {
+            throw error;
+          }
+    
+          if (data) {
+            setUsername(data.username);
+            setFullName(data.full_name);
+            setWebsite(data.website);
+            setAvatarUrl(data.avatar_url);
+            setUserID(data.id);
+          }
+        } catch (error) {
+          if (error instanceof Error) {
+            Alert.alert(error.message);
+          }
+        } finally {
+          setLoading(false);
+        }
+      }
+  return (
+    <View style={styles.DisplayContainer}>
+        <View >
+            <Text style={styles.UserWelcome}>Welcome {fullName}</Text>
+            <Text style={styles.UserWelcome}>Your username is {username}</Text>
+            <Text style={styles.UserWelcome}>Your website is {website}</Text>
+        </View>
+
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+DisplayContainer:{ 
+    alignItems: 'center',
+    height: '100%'
+},
+UserWelcome: {
+    fontSize: 20,
+    color: "Grey",
+    fontWeight: "bold",
+    textAlign: 'center'
+}
+})
